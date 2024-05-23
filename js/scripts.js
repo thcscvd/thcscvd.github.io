d67195e287091b8c8a6fba1f287730fe1dd31311
thienloc <ungthiendragon@gmail.com>
2024-05-23 08:38:42 +0700

    abc

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // text animation
+      // Define the text to be inserted
+  var newText = "Sinh Nhật Bác Hồ";
+
+  // Initialize Typed.js
+  var typed = new Typed('#text', {
+    strings: [newText],
+    typeSpeed: 50, // Typing speed in milliseconds
+    showCursor: false // Hide the cursor
+  });
+
+
+});
+$(document).ready(function(){
+    $('.portfolio-link').click(function(){
+      var imgSrc = $(this).find('img').attr('src');
+      $('#modalImage').attr('src', imgSrc);
+      $('#portfolioModal').modal('show');
+    });
+
+     });
